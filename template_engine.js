0ae7b262b52caa5786f9e46e58969e642d8ff5d8
ABHIRAJ SARDAR <abhirajsardar2003@gmail.com>
2023-12-26 00:29:06 +0530

    Update template_engine.js

--- template_engine.js
+++ template_engine.js
@@ -1,7 +1,7 @@
 const express=require('express');
 var app=express();
 
-app.set("view engine","ejs");
+app.set("view engine","ejs"); //by this we are telling node that we are going to use ejs 
 app.use(express.static('./public')); //this public path will automatically added at first of the path
 app.get("/",function(req,res){
     res.render("index"); //here we used render instead of send //here index means index.ejs under views
